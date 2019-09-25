44d955fd00e9c9864e00a4c3e32b6696f5c70e74
Andreas Hucks <andreas.hucks@sensiolabs.de>
2019-09-25 12:39:25 +0200

    add edit post test

--- src/AppBundle/Tests/Controller/BlogControllerTest.php
+++ src/AppBundle/Tests/Controller/BlogControllerTest.php
@@ -3,20 +3,44 @@
 namespace AppBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpKernel\Client;
 
 class BlogControllerTest extends WebTestCase
 {
+    /**
+     * @var Client
+     */
+    private $client;
+
     public function testEditPageLoads()
     {
-        $client = static::createClient(
+        $this->client->request('GET', '/en/admin/post/1');
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
+    }
+
+    public function testEditBlogTitle()
+    {
+        $this->client->request('GET', '/en/admin/post/1/edit');
+
+        $form = $this->client->getCrawler()->selectButton('Save changes')->form();
+        $this->client->submit($form, ['post[title]' => 'Test']);
+
+        $this->assertTrue($this->client->getResponse()->isRedirect());
+
+        $crawler = $this->client->followRedirect();
+        $msg = $crawler->filter('div.alert-success')->text();
+
+        $this->assertContains('Post updated successfully!', $msg);
+    }
+
+    protected function setUp()
+    {
+        $this->client = static::createClient(
             [],
             [
                 'PHP_AUTH_USER' => 'anna_admin',
                 'PHP_AUTH_PW' => 'kitten'
             ]
         );
-        $client->request('GET', '/en/admin/post/1');
-
-        $this->assertTrue($client->getResponse()->isSuccessful());
     }
 }
